a98853acc5bd34b717380a272347ed9f55d8ea47
Jey <andre.marqquez@gmail.com>
2022-08-03 01:28:39 -0300

    feat: post login
    create method post login and generate random token on every login

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require('express');
+const crypto = require('crypto');
+
+const talkerRouter = require('./Routers/talkerRouter');
+const { validLogin } = require('./Middlewares/validLogin');
 require('express-async-errors');
 
 const app = express();
 
 app.use(express.json());
 
-const talkerRouter = require('./Routers/talkerRouter');
-
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// não remova esse endpoint, e para o avaliador funcionar
+app.use('/talker', talkerRouter);
+
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.use('/talker', talkerRouter);
+app.post('/login', validLogin, (_req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  return res.status(200).json({ token });
+});
 
 app.use((err, _req, res, _next) => {
   res.status(err.code || 500).json({ message: err.message });
