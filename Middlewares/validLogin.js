d1bc5ffa1e03b5e80f710d1e1801959040c2068b
Jey <andre.marqquez@gmail.com>
2022-08-03 01:25:46 -0300

    Update validLogin.js
    Add messages referring to each validation type

--- Middlewares/validLogin.js
+++ Middlewares/validLogin.js
@@ -2,11 +2,20 @@ const validLogin = (req, res, next) => {
   const { email, password } = req.body;
   const validateEmailInputRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const isEmailValid = validateEmailInputRegex.test(email);
-
-  if (!isEmailValid) return res.status(401).json({ message: 'invalid email' });
-
-  if (!password) return res.status(401).json({ message: 'invalid password' });
-
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+  if (!isEmailValid) {
+    return res
+      .status(400)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
   next();
 };
 
